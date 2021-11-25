3b126a0f421da018b6aac3840b7d13665ded147a
MarePri <marko.cena@gmail.com>
2021-11-25 11:17:57 +0100

    try

--- src/App.js
+++ src/App.js
@@ -1,49 +1,22 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from "react-router-dom";
-import Tab from './comp/tab';
-import Home from './pages/Home'
-import About from './pages/About';
-import Features from './pages/Features';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header';
+import Routes from './Routes';
 import './App.css';
-
 
 function App() {
   return (
-   <Router>
-    <div className="app">
-      <div className="browser">
-        <div className="tabs">
-          <Tab> 
-            <a>Home</a> 
-          </Tab>
-          <Tab>
-             <a>About</a> 
-          </Tab>
-          <Tab> 
-            <a>Features</a> 
-          </Tab>      
-        </div>
+    <Router>
+      <div className="app">
+        <div className="browser">
+          <Header />
 
-        <div className="viewport">
-          <Route path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/features">
-            <Features />
-          </Route>
+          <div className="viewport">
+            <Routes />
+          </div>
         </div>
       </div>
-    </div>
-    
-   </Router>
+    </Router>
   );
 }
 
